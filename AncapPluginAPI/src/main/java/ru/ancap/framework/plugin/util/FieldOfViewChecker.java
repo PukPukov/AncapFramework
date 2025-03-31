bf43ab3977188ddce28b76893588a5420f4fdefa
PukPukov <kvasov.les@gmail.com>
2025-04-01 03:14:52 +0500

    add FieldOfViewChecker

---
+++ AncapPluginAPI/src/main/java/ru/ancap/framework/plugin/util/FieldOfViewChecker.java
@@ -0,0 +1,16 @@
+package ru.ancap.framework.plugin.util;
+
+import org.bukkit.Location;
+import org.bukkit.util.Vector;
+
+public class FieldOfViewChecker {
+    
+    public static boolean isInFrontOf(Location viewer, Vector viewDirection, int fieldOfView, Location viewed) {
+        Vector toDamager = viewed.toVector().subtract(viewer.toVector());
+        toDamager.normalize();
+        double angle = viewDirection.angle(toDamager);
+        angle = Math.toDegrees(angle);
+        return angle <= fieldOfView / 2.0;
+    }
+    
+}
