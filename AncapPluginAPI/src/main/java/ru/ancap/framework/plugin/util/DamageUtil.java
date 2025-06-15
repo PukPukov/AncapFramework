249d8b35445c5ff36e0fb9c270f8d76136f13558
PukPukov <kvasov.les@gmail.com>
2025-06-15 21:16:15 +0500

    1.7-pre.14

--- AncapPluginAPI/src/main/java/ru/ancap/framework/plugin/util/DamageUtil.java
+++ AncapPluginAPI/src/main/java/ru/ancap/framework/plugin/util/DamageUtil.java
@@ -3,18 +3,21 @@ package ru.ancap.framework.plugin.util;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Projectile;
+import org.jetbrains.annotations.NotNull;
 
 import javax.annotation.Nullable;
 
 public class DamageUtil {
     
-    public static @Nullable Player semiDeepFindDamager(Entity damager) {
-        if (damager instanceof Player player) return player;
+    public static @Nullable DeepDamager semiDeepFindDamager(Entity damager) {
+        if (damager instanceof Player player) return new DeepDamager(player, null);
         else if (damager instanceof Projectile projectile) {
-            if (projectile.getShooter() instanceof Player player) return player;
+            if (projectile.getShooter() instanceof Player player) return new DeepDamager(player, null);
             else return null;
         }
         return null;
     }
+    
+    public record DeepDamager(@NotNull Player player, @Nullable Projectile carrier) { }
     
 }
